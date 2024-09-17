568dd9cd4d7ef1c0641e877c936fd726bd044bbd
s-sarlak <shahrzadsarlak6@gmail.com>
2024-09-17 19:00:24 +0430

    Create h.w1-7.py

---
+++ h.w1-7.py
@@ -0,0 +1,61 @@
+#homework
+
+#Q1:
+l=[3,'name',True,3.2]
+print(l)
+#------------------------
+#Q2:
+d={
+    'key1':12,
+    'key2':'name',
+    'key3':True,
+    'key4':23.1
+}
+#------------------------
+#Q3:
+l=[]
+for i in range(0,11,2):
+    l.append(i)
+#-------------------------
+#Q4
+l=[]
+for i in range(0,11,2):
+    l.append(i)
+print(sum(l))
+#-------------------------
+#Q5:
+import numpy as np
+l=[]
+for i in range(0,11,2):
+    l.append(i)
+for i in range(len(l)):
+    np.prod(l)
+#------------------------
+#Q6:
+a=['a','b','c','d']
+b=[1,2,3,4]
+d={}
+d=dict(zip(a,b))
+
+#-----------------------
+#Q7:
+a=['a','b','c','d']
+b=[1,2,3,4]
+b.reverse()
+d={}
+d=dict(zip(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
